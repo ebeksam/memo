b0e2784780438219d55fa5ffe1c16782e4d9355a
Samuel Ebek <ebeksam@gmail.com>
2023-09-15 19:08:24 +0100

    Done with the memo App

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const firebaseConfig = {
   apiKey: "AIzaSyBSH3_E9sdh__YJdY_C4Vqrm1tr1gYUDbY",
   authDomain: "memo-app-5e3c8.firebaseapp.com",
@@ -15,100 +13,85 @@ firebase.initializeApp(firebaseConfig);
 //init services
 const db = firebase.firestore();
 
-// collection ref
 
 
-// const colRef = collection(db, 'memo')
-// const addButton = document.querySelector("#addButton")
-// const itemName = document.querySelector("#item-name")
-// const itemDes = document.querySelector("#item-des")
-// const ul = document.querySelector('ul');
- 
-  
-addButton.addEventListener("click", addTask);
 
-function addTask() {
+const memoItem = document.querySelector("#memo-items");
 
-const itemName = document.getElementById("item-name")
-const itemDes = document.getElementById("item-des")
+const form = document.querySelector("#add-items");
 
- // Modifying the text
- const taskName = itemName.value;
- const taskDes = itemDes.value;
+// create element and render memo 
 
-  db.collection("memo").add({
-    name: taskName,
-    desc: taskDes
-  })
+function renderMemo(doc) {
 
-   // Clear input values
-   itemName.value = "";
-   itemDes.value = "";
+  let li = document.createElement("li");
+  let name = document.createElement("span");
+  let desc = document.createElement("span");
+  let cross = document.createElement("div");
+
+  li.setAttribute("data-id", doc.id);
+  name.textContent = doc.data().name;
+  desc.textContent = doc.data().desc;
+  cross.textContent = "X"
 
 
-  // // Creating Element
-  // const li = document.createElement('li');
+  li.appendChild(name);
+  li.appendChild(desc);
+  li.appendChild(cross)
 
-  // // Adding Element
-  // ul.append(li);
+  memoItem.appendChild(li);
 
-  // li.classList.add('name');
-  // li.innerHTML = `<h2>${taskName}</h2><p>${taskDes}</p><button class="delete-btn">X</button>`;
+  //deleting data
 
-  // // Delete Button
-  // const deleteButton = li.querySelector(".delete-btn");
-  // deleteButton.addEventListener("click", function () {
-  //   li.remove();
-  // });
+  cross.addEventListener("click", e => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute("data-id");
 
- 
+    //find a doc on the dom
+    db.collection("memo")
+      .doc(id)
+      .delete();
+  });
+
+
 }
 
+// get data from firestore 
 
-function getItems(){
-
-  db.collection("memo").onSnapshot((snapshot) => {
-    console.log(snapshot)
-
-    let items = []
-    snapshot.docs.forEach((doc) => {
-     items.push({
-      id: doc.id,
-      ...doc.data()
-    })
-    })
-
-    generateItems(items)
-  })
-}
-
-function generateItems(items){
-
-  let itemHTML = ""
-  items.forEach((item) => {
-     console.log(item)
-    itemHTML += `
-    <li class="name">
-    <h2>${item.name}</h2>
-    <p>${item.desc}</p>
-    <button class="delete-btn">X</button>
-    </li>
-    `
-  })
-
-  document.querySelector(".memo-items").innerHTML = itemHTML
-}
-
-getItems()
-// 
-//   const taskName = itemName.value;
-//   const taskDes = itemDes.value;
-
-  
-//   .then(() => {
-//     console.log("Item added to Firestore");
+// db.collection('memo').get().then((snapshot) => {
+//   snapshot.docs.forEach(doc => {
+//     renderMemo(doc);
 //   })
-//   .catch(error => {
-//     console.error("Error adding item:", error);
-//   });
-// });
+// })
+
+
+// Adding data
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  db.collection("memo").add({
+    name: form.name.value,
+    desc: form.desc.value
+  });
+
+  form.name.value = '';
+  form.desc.value = '';
+
+})
+
+
+//  real time listener
+db.collection("memo").orderBy("desc").onSnapshot(snapshot => {
+
+  // We also rendered data in real-time using
+  let changes = snapshot.docChanges();
+
+  changes.forEach(change => {
+    if (change.type == "added") {
+      renderMemo(change.doc);
+    } else if (change.type == "removed") {
+      let li = memoItem.querySelector("[data-id=" + change.doc.id + "]");
+      memoItem.removeChild(li);
+    }
+  });
+});
